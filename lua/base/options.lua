65c02b78c546100458bffec282b602693a87ab54
anir183 <188557785+anir183@users.noreply.github.com>
2025-03-30 14:25:50 +0530

    refactor: remove unnecessary line

--- lua/base/options.lua
+++ lua/base/options.lua
@@ -42,7 +42,6 @@ opt.showmode = false;                                                           
 opt.scrolloff = 8;
 opt.ruler = false;
 opt.list = true;
-opt.listchars = {};
 local listchars = {
 	{ lead = "∙" },
 	-- vim.opt.tabstop returns a table instead of the required value
