72952bf0ce6ad72d158211e35b7a418494f1ea43
anir183 <188557785+anir183@users.noreply.github.com>
2025-04-06 00:35:11 +0530

    refactor: use custom function and aucmd instead of conform's format_after_save to show notification on format

--- lua/plugins/lists/dev_tools/formatter.lua
+++ lua/plugins/lists/dev_tools/formatter.lua
@@ -1,5 +1,27 @@
 --[[ formatter ]]
 
+-- try to format thrice before failing
+-- async failure is common for some reason
+local function format(count)
+	count = count and count or 1
+
+	require("conform").format({
+		async = true,
+	}, function(err, did_edit)
+		if not err then
+			if did_edit then
+				vim.notify("Formatted file!")
+			end
+		else
+			if count <= 3 then
+				format(count + 1)
+			else
+				vim.notify("Could not format!\n" .. err)
+			end
+		end
+	end)
+end
+
 return {
 	"stevearc/conform.nvim",
 	name = "conform",
@@ -10,14 +32,17 @@ return {
 		conform.setup({
 			formatters_by_ft = OPTS.formatters,
 			formatters = OPTS.custom_formatters,
-			format_on_save = { lsp_format = "never", timeout_ms = 5000 },
 		})
 
-		MAP(
-			{ "n", "v" },
-			"<leader>fm",
-			conform.format,
-			{ desc = "plugins/conform: format the file or selection" }
-		)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = AUGRP,
+			callback = function()
+				format()
+			end,
+		})
+
+		MAP({ "n", "v" }, "<leader>fm", function()
+			format()
+		end, { desc = "plugins/conform: format the file or selection" })
 	end,
 }
