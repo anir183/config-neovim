5af702d608a3000c6e3efc119f49f23109ce0ad8
anir183 <188557785+anir183@users.noreply.github.com>
2025-10-17 21:59:14 +0530

    feat: basic pre-config and settings schema

---
+++ lua/config-183/init.lua
@@ -0,0 +1,58 @@
+CONF_183 = {
+	augrp_name = {},
+	augrp = {},
+	functions = {},
+	settings = {},
+}
+
+-- load custom or if not available, default settings
+CONF_183.settings = vim.tbl_deep_extend(
+	"force",
+	require("config-183.settings.defaults"),
+	pcall(require, "config-183.settings.custom") and
+		require("config-183.settings.custom") or {}
+)
+
+-- auto command groups
+CONF_183.augrp_name = "augroup-183"
+CONF_183.augrp = vim.api.nvim_create_augroup(CONF_183.augrp_name, {
+	clear = true
+})
+
+-- verbose printing
+CONF_183.functions.vprint = function(...)
+	if not (vim.g.VERBOSE or CONF_183.settings.verbose_mode) then
+		return
+	end
+	vim.print(...)
+end
+
+-- mapping functions
+CONF_183.functions.map = function(mode, lhs, rhs, desc, opts)
+	opts = opts or {}
+	opts.desc = desc
+	vim.keymap.set(mode, lhs, rhs, opts)
+end
+CONF_183.functions.nmap = function(lhs, rhs, desc, opts)
+	opts = opts or {}
+	opts.desc = desc
+	vim.keymap.set("n", lhs, rhs, opts)
+end
+
+-- when opened in vscode via vscode-neovim extension
+if vim.g.vscode then
+	print("Launched in VSCode Mode")
+	-- require("config-183.vscode")
+end
+
+-- minimal mode
+if vim.g.MINIMAL or CONF_183.settings.minimal_mode then
+	print("Launched in Minimal Mode")
+	-- require("config-183.minimal")
+	return
+end
+
+-- normal configuration
+-- require("config-183.base")
+-- require("config-183.custom")
+-- require("config-183.plugin")
---
+++ lua/config-183/settings/defaults.lua
@@ -0,0 +1,172 @@
+--[[ default.lua: default configuration settings ]]
+
+return {
+	-- neovim modes
+	minimal_mode = false,
+	verbose_mode = false,
+
+	-- size of netrw sidebar
+	netrw_winsize = 17,
+
+	-- prefix for custom commands
+	cmd_pre = "CMD",
+
+	-- extra plugins to be installed
+	-- uses generic lazy syntax
+	plugins = {
+		-- {
+		--     "plugin/link",
+		--     name = "",
+		--     config = function() end,
+		--     ...
+		-- }
+	},
+
+	-- disable all other plugins to create a plugin testing environment
+	-- false or nil to use normal plugins
+	test_plugins = false,
+
+	-- to be executed on the VimEnter event, after config is loaded
+	after = function()
+		-- vim.opt.bla = bla;
+	end,
+
+	-- lsps and their setup
+	-- lsp names to be taken from mason
+	lsps = {
+		-- lsp_name = { lspopts }, -- alternatively false to only install and not setup
+	},
+
+	-- formatters and their setup
+	-- formatter names to be taken from conform (https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters)
+	formatters = {
+		-- filetype = { "formatter1", "formatter2", stop_after_first = false }
+	},
+
+	-- setup custom formatters not available on mason or conform
+	-- follow https://github.com/stevearc/conform.nvim?tab=readme-ov-file#customizing-formatters
+	custom_formatters = {
+		-- formatter_name = {
+		--     command = "whatever",
+		--     env = ...,
+		--     ...
+		-- }
+	},
+
+	-- linters and their setup
+	-- linter names to be taken from nvim-lint (https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#available-linters)
+	linters = {
+		-- filetype = { "linter1", "linter2" }
+	},
+
+	-- setup custom formatters not available on mason or nvim-lint
+	-- follow https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#custom-linters
+	custom_linters = {
+		-- linter_name = {
+		--     cmd = "whatever",
+		--     stdin = true,
+		--     args = {},
+		--     ...
+		-- }
+	},
+
+	-- daps and their setup
+	-- dap names to be taken from mason
+	-- setup examples in https://github.com/jay-babu/mason-nvim-dap.nvim?tab=readme-ov-file#advanced-customization
+	daps = {
+		-- dap1_name = function(config) <handler code> end,
+		-- dap2_name = nil,
+	},
+
+	-- setup custom daps not available on mason
+	-- follow https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+	custom_daps = {
+		-- {
+		--     adapter_name = {
+		--         type = "",
+		--         command = "",
+		--         args = {},
+		--         ...
+		--     },
+		--     configuration_name = {
+		--         type = "",
+		--         name = "",
+		--         ...
+		--     }
+		-- },
+	},
+
+	-- add new filetypes according to extension, filename or regex patterns
+	additional_filetypes = {
+		extension = {
+			-- ext = "filetype",
+		},
+		filename = {
+			-- ["name.ext"] = "filetype",
+		},
+		pattern = {
+			-- ["regex_pattern"] = "filetype",
+		},
+	},
+
+	-- setup custom treesitter not included with treesitter
+	treesitter_parsers = {
+		-- parser_name = {
+		--     install_info = {
+		--         url = "",
+		--         branch = "",
+		--         files = { "", "" },
+		--     },
+		-- filetype = "",
+		-- },
+	},
+
+	-- patterns and file types to obscure for safety
+	cloak_patterns = {
+		-- {
+		--     file_pattern = { "", "" },
+		--     cloak_pattern = { "", "" },
+		--     replace = nil
+		-- },
+	},
+
+	-- toggle different configuration modules on and off
+	-- toggling a module-group turns off all modules under it
+	-- module_toggles = { } -- keeps defaults
+	module_toggles = {
+		-- base = false -- turns all base-group modules off
+		-- base = { } -- uses defaults
+
+		base = {
+			keymaps = true,
+			options = true,
+			autocmds = true,
+		},
+		custom = {
+			commands = true,
+			keymaps = true,
+			netrw = true,
+			statusline = true,
+		},
+		minimal = {
+			keymaps = true,
+			options = true,
+		},
+		vscode = {
+		},
+		plugins = {
+			aesthetics = true,
+			dependencies = true,
+			essentials = true,
+			quality_of_life = true,
+			dev_tools = {
+				completion = true,
+				dap = true,
+				formatter = true,
+				linting = true,
+				lsp = true,
+				manager = true,
+			},
+		}
+	}
+}
