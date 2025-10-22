c9751a189235a90a6f363560bdc16418479de94a
anir183 <188557785+anir183@users.noreply.github.com>
2025-10-22 15:49:27 +0530

    fix: only turn off breadcrumbs if unavailable to remove flicker

--- lua/config-183/plugin/lists/quality_of_life/breadcrumbs.lua
+++ lua/config-183/plugin/lists/quality_of_life/breadcrumbs.lua
@@ -16,11 +16,8 @@ return {
 		vim.api.nvim_create_autocmd("CursorMoved", {
 			group = CONF_183.augrp,
 			callback = function()
-				-- get breadcrumbs location and trim it
-				local location = (navic.get_location() or ""):gsub("%s+", "")
-
-				-- hide winbar if no data availabel or data is empty
-				if navic.is_available() and location ~= "" then
+				-- hide winbar if breadcrumbs are unavailable
+				if navic.is_available() then
 					vim.opt.winbar = "%{%v:lua.require(\"nvim-navic\")"
 						.. ".get_location()%}"
 				else
