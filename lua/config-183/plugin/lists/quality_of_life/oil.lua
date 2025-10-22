3b7950c8d1045b08e9f8812d0a9b31c3ce9d5225
anir183 <188557785+anir183@users.noreply.github.com>
2025-10-22 16:16:50 +0530

    minor: dont prompt on selecting new entry in oil buffer

--- lua/config-183/plugin/lists/quality_of_life/oil.lua
+++ lua/config-183/plugin/lists/quality_of_life/oil.lua
@@ -7,6 +7,7 @@ return {
 	opts = {
 		default_file_explorer = false,
 		skip_confirm_for_simple_edits = true,
+		prompt_save_on_select_new_entry = false,
 		view_options = {
 			show_hidden = true,
 		},
