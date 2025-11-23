744fe8c73aacb2beba1dc555ac462affeb79bd0b
anir183 <188557785+anir183@users.noreply.github.com>
2025-11-23 18:48:14 +0530

    plugin(gitsigns): add info to statusline

--- lua/config-183/plugin/lists/quality_of_life/git_signs.lua
+++ lua/config-183/plugin/lists/quality_of_life/git_signs.lua
@@ -6,8 +6,70 @@ return {
 	lazy = false,
 	opts = {
 		signcolumn = false,
-		numhl = true,
 	},
+	config = function(_, opts)
+		require("gitsigns").setup(opts)
+
+		-- git information function for statusline
+		CONF_183.statusline.component_functions.gitinfo = function()
+			local git_info = vim.b.gitsigns_status_dict
+			if not git_info or git_info.head == "" then
+				return ""
+			end
+
+			local head = git_info.head
+			local added = git_info.added and (" +" .. git_info.added) or ""
+			local changed = git_info.changed and (" ~" .. git_info.changed)
+				or ""
+			local removed = git_info.removed and (" -" .. git_info.removed)
+				or ""
+			if git_info.added == 0 then
+				added = ""
+			end
+			if git_info.changed == 0 then
+				changed = ""
+			end
+			if git_info.removed == 0 then
+				removed = ""
+			end
+
+			local fmt_str = "%%#%s#%s%%*"
+			return table.concat({
+				fmt_str:format("Exception", "["),
+				fmt_str:format("Exception", head),
+				fmt_str:format("DiagnosticSignOk", added),
+				fmt_str:format("DiagnosticSignWarn", changed),
+				fmt_str:format("DiagnosticSignError", removed),
+				fmt_str:format("Exception", "]"),
+			})
+		end
+
+		-- add gitinfo to the statusline arrangement
+		local component = CONF_183.statusline.get_component
+		CONF_183.statusline.arrangement = {
+			-- left
+			component("mode"),
+			component("diagnostics"),
+			" ",
+			"%r",
+			"%w",
+			"%h",
+			"%m",
+
+			"%=",
+
+			-- right
+			component("gitinfo"),
+			" ",
+			component("filename"),
+			" ",
+			component("indent"),
+			" ",
+			component("position"),
+		}
+		vim.opt_global.statusline =
+			table.concat(CONF_183.statusline.arrangement, "")
+	end,
 	keys = {
 		-- navigate hunks
 		{
